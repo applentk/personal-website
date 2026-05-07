e59fc944be89934fe0ccd6160e5aed8e222d1146
applentk <tanoobguy12@gmail.com>
2026-05-07 13:00:25 +0700

    fix: replace dynamic S3 hostname with static CDN hostname in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,3 @@
-import "dotenv/config"
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -6,8 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        // eslint-disable-next-line @typescript-eslint/no-extra-non-null-assertion
-        hostname: process.env.NEXT_PUBLIC_S3_PUBLIC_URL!?.replace("https://", ""),
+        hostname: "cdn.applentk.com",
         port: "",
         pathname: "/**",
       },
