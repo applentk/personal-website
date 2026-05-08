d020a7323afa5509a26910cc1924ddfc124d0058
applentk <tanoobguy12@gmail.com>
2026-05-09 03:22:15 +0700

    fix: add missing remote pattern for image optimization in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         hostname: "cdn.applentk.com",
         pathname: "/**"
       },
+      {
+        protocol: "https",
+        hostname: "https://pub-22c8d20aa6f24b3895a2cdb65854b43f.r2.dev",
+        pathname: "/**"
+      }
     ],
   }
 };
